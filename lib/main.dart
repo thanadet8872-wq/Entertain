9c6789f1b68ab95e2de6314bc3321519123ef4dc
Tanadat Toongphanassak <tanadattoongphanassak@Mac-mini-khxng-Tanadat.local>
2025-10-10 16:58:35 +0700

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/material.dart';
+import 'get started.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Entertain',
+      theme: ThemeData(
+        primarySwatch: Colors.amber,
+        visualDensity: VisualDensity.adaptivePlatformDensity,
+      ),
+      home: GetStartedScreen(),
+      debugShowCheckedModeBanner: false,
+    );
+  }
+}
